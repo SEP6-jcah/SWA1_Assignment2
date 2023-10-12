fe64c9144bc7155dd360605a45e73947cb8f1f5f
carrolljody <75698201+carrolljody@users.noreply.github.com>
2023-10-12 06:13:22 +0200

    initial board tests passed

--- functional/src/board.ts
+++ functional/src/board.ts
@@ -10,20 +10,57 @@ export type Match<T> = {
     positions: Position[]
 }    
 
-export type Board<T> = ?;
+export type Board<T> = {
+  data: (T | undefined)[][];
+  generator: Generator<T>;
+  matches: Match<T>[];
+  width: number;
+  height: number;
+};
 
-export type Effect<T> = ?;
+export type Effect<T> = {}
 
-export type MoveResult<T> = {
-    board: Board<T>,
-    effects: Effect<T>[]
-}    
+
+export type MoveResult<T> = {}    
 
 export function create<T>(generator: Generator<T>, width: number, height: number): Board<T> {
-}    
+  const board: Board<T> = {
+    data: [],
+    generator: generator,
+    matches: [],
+    width: width,
+    height: height
+  };
+  
+  for (let row = 0; row < height; row++) {
+    board.data[row] = new Array(width);
+    for (let col = 0; col < width; col++) {
+      board.data[row][col] = generator.next();
+    }
+  }
+  
+  return board;
+}
+
+export function positions<T>(board: Board<T>): Position[] {
+  const positions: Position[] = [];
+    for (let row = 0; row < board.height; row++) {
+      for (let col = 0; col < board.width; col++) {
+        positions.push({ row, col });
+      }
+    }
+  return positions;
+}
 
 export function piece<T>(board: Board<T>, p: Position): T | undefined {
-}    
+  const { row, col } = p;
+
+  if (row >= 0 && row < board.height && col >= 0 && col < board.width) {
+    return board.data[row][col];
+  }
+
+  return undefined;
+}
 
 export function canMove<T>(board: Board<T>, first: Position, second: Position): boolean {
 }
