349f632ed63da76fee6a4162ec546e78b8b3f4d1
carrolljody <75698201+carrolljody@users.noreply.github.com>
2023-12-27 23:02:35 +0100

    Update profile.component.ts

--- match_game_angular/src/app/components/profile/profile.component.ts
+++ match_game_angular/src/app/components/profile/profile.component.ts
@@ -1,5 +1,3 @@
-// profile.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from '../../services/user.service';
